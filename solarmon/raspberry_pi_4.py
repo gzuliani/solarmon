c751d3b57d7a843d44c8ee806eef8057f7840853
Gianfranco <Gianfranco Zuliani>
2024-02-25 16:17:56 +0100

    raspberry_pi_4: disk usage for every non-tmp partition and rssi for every wifi interface

--- solarmon/raspberry_pi_4.py
+++ solarmon/raspberry_pi_4.py
@@ -6,6 +6,48 @@ import subprocess
 import time
 
 
+class FileSystem:
+
+    def __init__(self, mount_point, name, type):
+        self.mount_point = mount_point
+        self.name = name
+        self.type = type
+
+    def list():
+        file_systems = []
+        df_output = Command('df -T').run().decode()
+        # skip the the heading
+        for line in df_output.split('\n')[1:]:
+            if not line:
+                break
+            tokens = line.split()
+            if len(tokens) != 7:
+                break
+            file_systems.append(FileSystem(
+                tokens[6], # mount point
+                tokens[0], # file system
+                tokens[1], # file system type
+            ))
+        return file_systems
+
+
+class WirelessInterface:
+
+    def __init__(self, name, ssid):
+        self.name = name
+        self.ssid = ssid
+
+    def list():
+        ifaces = []
+        iwconfig_output = Command('iwconfig').run().decode()
+        for line in iwconfig_output.split('\n'):
+            match = re.search('^([\w]+)\s+ IEEE.*ESSID:"([^"]*)"', line)
+            if match:
+                name, ssid = match.group(1), match.group(2)
+                ifaces.append(WirelessInterface(name, ssid))
+        return ifaces
+
+
 class Command:
 
     def __init__(self, command):
@@ -23,7 +65,7 @@ class Command:
 class CpuLoadPercent:
 
     def __init__(self):
-        self.name = 'cpu-load-percent'
+        self.name = 'cpu-load'
 
     def read(self):
         return psutil.cpu_percent(.1)
@@ -32,7 +74,7 @@ class CpuLoadPercent:
 class CpuTemperature:
 
     def __init__(self):
-        self.name = 'cpu-temperature'
+        self.name = 'cpu-t'
 
     def read(self):
         return int(open('/sys/class/thermal/thermal_zone0/temp').read())/1000
@@ -41,7 +83,7 @@ class CpuTemperature:
 class RamUsedPercent:
 
     def __init__(self):
-        self.name = 'ram-used-percent'
+        self.name = 'ram-usage'
 
     def read(self):
         memory = psutil.virtual_memory()
@@ -50,8 +92,8 @@ class RamUsedPercent:
 
 class DiskUsedPercent:
 
-    def __init__(self, path):
-        self.name = 'disk-used-percent'
+    def __init__(self, name, path):
+        self.name = name
         self._path = path
 
     def read(self):
@@ -61,9 +103,10 @@ class DiskUsedPercent:
 
 class WifiSignalStrength:
 
-    def __init__(self):
-        self.name = 'wifi-signal-strength'
-        self._command = Command('iwconfig')
+    def __init__(self, name, interface):
+        self.name = name
+        self.interface = interface
+        self._command = Command(f'iwconfig {self.interface}')
         self._signal_level = re.compile(b'Signal level=(-\d+)')
 
     def read(self):
@@ -90,9 +133,22 @@ class RaspberryPi4:
             CpuLoadPercent(),
             CpuTemperature(),
             RamUsedPercent(),
-            DiskUsedPercent('/'),
-            WifiSignalStrength(),
         ]
+        # file system usage
+        for fs in [
+            x for x in FileSystem.list()
+                if x.type not in ['vfat', 'squashfs']
+                    and not x.name in ['tmpfs', 'devtmpfs', 'udev']]:
+            self._params.append(
+                DiskUsedPercent(
+                    f'disk-usage_{fs.mount_point}',
+                    fs.mount_point))
+        # wifi signal strength
+        for iface in WirelessInterface.list():
+            self._params.append(
+                WifiSignalStrength(
+                    f'rssi_{iface.ssid}',
+                    iface.name))
 
     def params(self):
         return self._params
