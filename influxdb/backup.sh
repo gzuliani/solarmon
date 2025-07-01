3001cc3e8519d4d99bb0ed852a05e649282be82c
Gianfranco <Gianfranco Zuliani>
2025-07-01 21:14:20 +0200

    fixed the backup script for influxdb

--- influxdb/backup.sh
+++ influxdb/backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# suggested cron schedule: '0 3 1 1,4,7,10 *' (every 3 months at 03:00)
+# suggested cron schedule: '0 0 1 * *' (at 00:00 on day-of-month 1)
 
 TARGET_DIR=/home/pi/solarmon/influxdb/backup
 mkdir -p $TARGET_DIR
 TARGET_FILE=$TARGET_DIR/$(date '+%Y%m%d%H%M')
 echo Creating InfluxDB backup $TARGET_FILE...
-influx backup $TARGET_FILE -t <ROOT-TOKEN>
+/usr/local/bin/influx backup $TARGET_FILE -t <ROOT-TOKEN>
