5ca419cc3c70249e79cc20737dfce1e87ff2cf60
Gianfranco <Gianfranco Zuliani>
2026-07-01 19:34:17 +0200

    updated the backup script

--- influxdb/backup.sh
+++ influxdb/backup.sh
@@ -2,8 +2,11 @@
 
 # suggested cron schedule: '0 0 1 * *' (at 00:00 on day-of-month 1)
 
-TARGET_DIR=/home/pi/solarmon/influxdb/backup
-mkdir -p $TARGET_DIR
-TARGET_FILE=$TARGET_DIR/$(date '+%Y%m%d%H%M')
-echo Creating InfluxDB backup $TARGET_FILE...
-/usr/local/bin/influx backup $TARGET_FILE -t <ROOT-TOKEN>
+INFLUX=/usr/local/bin/influx
+BASE_DIR=/home/pi/solarmon/influxdb/backup
+mkdir -p $BASE_DIR
+BACKUP_DIR=$BASE_DIR/$(date '+%Y%m%d%H%M')
+echo Creating InfluxDB backup $BACKUP_DIR...
+$INFLUX backup $BACKUP_DIR -t <ROOT-TOKEN>
+tar -zcvpf $BACKUP_DIR.tar.gz $BACKUP_DIR
+rm -rf $BACKUP_DIR
